dd379343191905a5de596b35f38e820b9e753b41
Ryan McCartan <ryanjmccartan@gmail.com>
2021-03-26 11:24:35 -0500

    Added conditional to display if data is not null

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -9,14 +9,18 @@ import { EventEmitter } from 'events'
             <div>Date: {{event.date}}</div>
             <div>Time: {{event.time}}</div>
             <div>Price: \${{event.price}}</div>
-            <div>
+            <div *ngIf="event?.location">
                 <span>Location: {{event.location.address}}</span>
                 <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            </div>
+            <div *ngIf="event?.onlineUrl">
+                Online URL: {{event?.onlineUrl}}
             </div>
         </div>
     `,
     styles: [`
-        .pad-left: {margin-left 10px;}
+        .thumbnail {min-height: 210px;}
+        .pad-left {margin-left 10px;}
         .well div {color: #bbb;}
     `]
 })
