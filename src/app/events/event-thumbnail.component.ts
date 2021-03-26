04470bb219212b07c39de50d976c1f1f1468e4ad
Ryan McCartan <ryanjmccartan@gmail.com>
2021-03-26 13:50:51 -0500

    Added conditonal styling for early start times

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,7 +7,12 @@ import { EventEmitter } from 'events'
         <div class="well hoverwell thumbnail">
             <h2>{{event.name}}</h2>
             <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
+            <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+                Time: {{event.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
             <div>Price: \${{event.price}}</div>
             <div *ngIf="event?.location">
                 <span>Location: {{event.location.address}}</span>
@@ -19,6 +24,8 @@ import { EventEmitter } from 'events'
         </div>
     `,
     styles: [`
+        .green {color: #003300 !important;}
+        .bold {font-weight: bold;}
         .thumbnail {min-height: 210px;}
         .pad-left {margin-left 10px;}
         .well div {color: #bbb;}
@@ -27,5 +34,10 @@ import { EventEmitter } from 'events'
 
 export class EventThumbnailComponent {
     @Input() event:any
+
+    getStartTimeClass() {
+        const isEarlyStart = this.event && this.event.time === '8:00 am'
+        return {green: isEarlyStart, bold: isEarlyStart}
+    }
     
 }
